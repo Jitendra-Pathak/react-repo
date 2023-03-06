6bd158d848d3baa28835d021ee33f86ed1994f4a
FA063959 <jitendra.pathak@infoedge.com>
2023-03-06 15:59:22 +0530

    api component push .................

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const Badge = (props) => {
+  const [data, setData] = React.useState('default value');
 
   React.useEffect(()=>{
     console.log('use effect');
+    setData('updated value @@@@@@@@@@@@@@@@@@@@@@@');
   },[])
 
    return (
     <div className={`badge ${!props.value ? 'badge--none' :''} `}>
       <h4 className="heavy">{props.value || 0}</h4>
+      {data}
     </div>
    );
 }
