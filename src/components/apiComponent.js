9fe070a3b97cb693d83047d285f68078ed0b19bb
FA063959 <jitendra.pathak@infoedge.com>
2023-03-06 13:23:57 +0530

    api component push .................

--- src/components/apiComponent.js
+++ src/components/apiComponent.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Badge from './Badge';
 
 const url = 'https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2014-02-01&dim=0.15&api_key=DEMO_KEY';
