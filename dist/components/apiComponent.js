6e5e77caff7b66f737eec811436dbc4f89e5aaaf
FA063959 <jitendra.pathak@infoedge.com>
2023-03-06 15:46:34 +0530

    api component push .................

--- dist/components/apiComponent.js
+++ dist/components/apiComponent.js
@@ -1,21 +1,15 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
 
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 
 var _Badge = _interopRequireDefault(require("./Badge"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
@@ -32,12 +26,12 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 var url = 'https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2014-02-01&dim=0.15&api_key=DEMO_KEY';
 
 var ApiComp = function ApiComp() {
-  var _useState = (0, _react.useState)(''),
-      _useState2 = _slicedToArray(_useState, 2),
-      imgUrl = _useState2[0],
-      setData = _useState2[1];
+  var _React$useState = _react["default"].useState(''),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      imgUrl = _React$useState2[0],
+      setData = _React$useState2[1];
 
-  (0, _react.useEffect)(function () {
+  _react["default"].useEffect(function () {
     fetch(url).then(function (res) {
       return res.json();
     }).then(function (res) {
@@ -45,6 +39,7 @@ var ApiComp = function ApiComp() {
       setData(res.url);
     });
   }, []);
+
   return /*#__PURE__*/_react["default"].createElement("div", {
     className: "apiComp"
   }, /*#__PURE__*/_react["default"].createElement("hr", null), /*#__PURE__*/_react["default"].createElement("img", {
--- src/components/apiComponent.js
+++ src/components/apiComponent.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Badge from './Badge';
 
 const url = 'https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2014-02-01&dim=0.15&api_key=DEMO_KEY';
 
 const ApiComp = () => {
-    const [imgUrl, setData] = useState('');
+    const [imgUrl, setData] = React.useState('');
 
-    useEffect(() => {
+    React.useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(res => {
