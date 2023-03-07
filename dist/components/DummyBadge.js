2760fb968af48bf53718817788d82485b8ce72e3
FA063959 <jitendra.pathak@infoedge.com>
2023-03-07 10:57:53 +0530

    api component push .................

---
+++ dist/components/DummyBadge.js
@@ -0,0 +1,16 @@
+"use strict";
+"use client";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _Badge = require("./Badge");
+
+var DummyBadge = function DummyBadge() {
+  return /*#__PURE__*/React.createElement(_Badge.Badge, null);
+};
+
+var _default = DummyBadge;
+exports["default"] = _default;
---
+++ src/components/DummyBadge.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { Badge } from './Badge';
+
+const DummyBadge = () => {
+    return <Badge />
+};
+
+export default DummyBadge;
