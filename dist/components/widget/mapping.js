d9dd291a8d466552f25762ac18abb5c2145d0772
FA063959 <jitendra.pathak@infoedge.com>
2023-03-07 17:04:14 +0530

    api component push .................

--- dist/components/widget/mapping.js
+++ dist/components/widget/mapping.js
@@ -5,13 +5,17 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
+var _react = _interopRequireDefault(require("react"));
+
 var _Badge = require("../Badge");
 
 var _DummyBadge = require("../DummyBadge");
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 var Mapping = {
-  'test_wdgt_v0': /*#__PURE__*/React.createElement(_Badge.Badge, null),
-  'wdgt_badge': /*#__PURE__*/React.createElement(_DummyBadge.DummyBadge, null)
+  'test_wdgt_v0': /*#__PURE__*/_react["default"].createElement(_Badge.Badge, null),
+  'wdgt_badge': /*#__PURE__*/_react["default"].createElement(_DummyBadge.DummyBadge, null)
 };
 var _default = Mapping;
 exports["default"] = _default;
