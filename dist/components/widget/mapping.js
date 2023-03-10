eb47f81deab6d8b5e956ae17c46f4c1e10e7018a
FA063959 <jitendra.pathak@infoedge.com>
2023-03-10 17:19:19 +0530

    api component push .................

--- dist/components/widget/mapping.js
+++ dist/components/widget/mapping.js
@@ -7,20 +7,15 @@ exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _Badge = require("../Badge");
+var _Badge = _interopRequireDefault(require("../Badge"));
 
-var _DummyBadge = require("../DummyBadge");
+var _DummyBadge = _interopRequireDefault(require("../DummyBadge"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// import ReactDOMServer from 'react-dom/server'
 var Mapping = {
-  'test_wdgt_v0': /*#__PURE__*/_react["default"].createElement(_Badge.Badge, null),
-  'wdgt_badge': /*#__PURE__*/_react["default"].createElement(_DummyBadge.DummyBadge, null)
-}; // const Mapping = {
-//     'test_wdgt_v0':  ReactDOMServer.renderToString(<Badge />),
-//     'wdgt_badge':  ReactDOMServer.renderToString(<DummyBadge />),
-// };
-
+  'test_wdgt_v0': /*#__PURE__*/_react["default"].createElement(_Badge["default"], null),
+  'wdgt_badge': /*#__PURE__*/_react["default"].createElement(_DummyBadge["default"], null)
+};
 var _default = Mapping;
 exports["default"] = _default;
