cb579f46f4b778fabe61935e601a855e20573b6d
FA063959 <jitendra.pathak@infoedge.com>
2023-03-10 15:01:05 +0530

    api component push .................

--- dist/components/widget/mapping.js
+++ dist/components/widget/mapping.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { Badge, DummyBadge } from "../../../src/index";
-// import { DummyBadge } from "../DummyBadge";
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
 
-const Mapping = {
-    'test_wdgt_v0': Badge,
-    'wdgt_badge': DummyBadge
-  };
+var _react = _interopRequireDefault(require("react"));
 
-export default Mapping;
+var _Badge = require("../Badge");
+
+var _DummyBadge = require("../DummyBadge");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// const Mapping = {
+//     'test_wdgt_v0': <Badge />,
+//     'wdgt_badge': <DummyBadge />
+//   };
+var Mapping = function Mapping(path) {
+  switch (path) {
+    case 'test_wdgt_v0':
+      return _Badge.Badge;
+      break;
+
+    case 'wdgt_badge':
+      return _DummyBadge.DummyBadge;
+      break;
+
+    default:
+      return _Badge.Badge;
+  }
+};
+
+var _default = Mapping;
+exports["default"] = _default;
--- src/components/widget/mapping.js
+++ src/components/widget/mapping.js
@@ -4,9 +4,25 @@ import { Badge } from "../Badge";
 import { DummyBadge } from "../DummyBadge";
 
 
-const Mapping = {
-    'test_wdgt_v0': <Badge />,
-    'wdgt_badge': <DummyBadge />
-  };
+// const Mapping = {
+//     'test_wdgt_v0': <Badge />,
+//     'wdgt_badge': <DummyBadge />
+//   };
+
+
+const Mapping = (path) => {
+  switch(path) {
+    case 'test_wdgt_v0':
+      return Badge;
+      break;
+
+    case 'wdgt_badge':
+      return DummyBadge
+      break;
+
+    default:
+      return Badge;
+  }
+}
 
 export default Mapping;
