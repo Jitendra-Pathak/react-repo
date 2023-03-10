32f45b00187edc74bec47e3c0098231772bdb482
FA063959 <jitendra.pathak@infoedge.com>
2023-03-10 15:52:57 +0530

    api component push .................

--- dist/components/widget/mapping.js
+++ dist/components/widget/mapping.js
@@ -7,6 +7,8 @@ exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _server = _interopRequireDefault(require("react-dom/server"));
+
 var _Badge = require("../Badge");
 
 var _DummyBadge = require("../DummyBadge");
@@ -14,8 +16,8 @@ var _DummyBadge = require("../DummyBadge");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var Mapping = {
-  'test_wdgt_v0': /*#__PURE__*/_react["default"].createElement(_Badge.Badge, null),
-  'wdgt_badge': /*#__PURE__*/_react["default"].createElement(_DummyBadge.DummyBadge, null)
+  'test_wdgt_v0': _server["default"].renderToString( /*#__PURE__*/_react["default"].createElement(_Badge.Badge, null)),
+  'wdgt_badge': _server["default"].renderToString( /*#__PURE__*/_react["default"].createElement(_DummyBadge.DummyBadge, null))
 };
 var _default = Mapping;
 exports["default"] = _default;
--- src/components/widget/mapping.js
+++ src/components/widget/mapping.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server'
 
 import { Badge } from "../Badge";
 import { DummyBadge } from "../DummyBadge";
 
 
 const Mapping = {
-    'test_wdgt_v0': <Badge />,
-    'wdgt_badge': <DummyBadge />
+    'test_wdgt_v0':  ReactDOMServer.renderToString(<Badge />),
+    'wdgt_badge':  ReactDOMServer.renderToString(<DummyBadge />),
+    
 };
 
 export default Mapping;
