94dde34baeb48ba52b89912ba9359d0dd0ed9806
FA063959 <jitendra.pathak@infoedge.com>
2023-03-10 11:39:22 +0530

    api component push .................

--- dist/components/widget/mapping.js
+++ dist/components/widget/mapping.js
@@ -1,12 +1,10 @@
-import React from 'react';
-
 import { Badge } from "../Badge";
 import { DummyBadge } from "../DummyBadge";
 
 
 const Mapping = {
-    'test_wdgt_v0': <Badge />,
-    'wdgt_badge': <DummyBadge />
+    'test_wdgt_v0': Badge,
+    'wdgt_badge': DummyBadge
   };
 
 export default Mapping;
